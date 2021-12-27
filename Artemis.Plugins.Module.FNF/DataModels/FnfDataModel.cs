d8c3e562b66a54c8331777ef01c1fc34bb4ef22d
niko <skedgyedgy@gmail.com>
2021-12-26 19:31:22 -0500

    set up fnf plugin

---
+++ Artemis.Plugins.Module.FNF/DataModels/FnfDataModel.cs
@@ -0,0 +1,34 @@
+﻿using Artemis.Core.Modules;
+using System.Collections.Generic;
+
+namespace Artemis.Plugins.Module.FNF.DataModels {
+    public class FnfDataModel : DataModel {
+        public FnfDataModel () {
+            //PluginSubDataModel = new FnfSubDataModel ();
+        }
+
+        // Your datamodel can have regular properties and you can annotate them if you'd like
+        [DataModelProperty (Name = "Player health", Description = "How much health boyfriend has.")]
+        public int BoyfriendHealth { get; set; }
+
+        /*// You can even have classes in your datamodel, just don't forget to instantiate them ;)
+        [DataModelProperty (Name = "A class within the datamodel")]
+        public FnfSubDataModel PluginSubDataModel { get; set; }*/
+    }
+
+    /*public class FnfSubDataModel {
+        public FnfSubDataModel () {
+            ListOfInts = new List<int> { 1, 2, 3, 4, 5 };
+        }
+
+        // You don't need to annotate properties, they will still show up 
+        public float FloatyFloat { get; set; }
+
+        // You can even have a list!
+        public List<int> ListOfInts { get; set; }
+
+        // If you don't want a property to show up in the datamodel, annotate it with DataModelIgnore
+        [DataModelIgnore]
+        public string MyDarkestSecret { get; set; }
+    }*/
+}
---
+++ Artemis.Plugins.Module.FNF/FnfModule.cs
@@ -0,0 +1,27 @@
+﻿using Artemis.Core;
+using Artemis.Core.Modules;
+using Artemis.Plugins.Module.FNF.DataModels;
+using SkiaSharp;
+using System.Collections.Generic;
+
+namespace Artemis.Plugins.Module.FNF {
+    [PluginFeature (Name = "Friday Night Funkin' Module", Icon = "Music")]
+    public class FnfModule : Module<FnfDataModel> {
+        public override List<IModuleActivationRequirement> ActivationRequirements => null; // might add this, probably won't just to be on the safe side when it comes to mods
+
+        public override void Enable () {
+        }
+
+        public override void Disable () {
+        }
+
+        public override void ModuleActivated (bool isOverride) {
+        }
+
+        public override void ModuleDeactivated (bool isOverride) {
+        }
+
+        public override void Update (double deltaTime) {
+        }
+    }
+}
