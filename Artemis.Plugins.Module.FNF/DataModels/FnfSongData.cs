1fa0134083a0c4e3f5785fd4a0f9793673a15b69
niko <skedgyedgy@gmail.com>
2021-12-27 16:49:06 -0500

    made it so health values can actually be edited by json parser thing

--- Artemis.Plugins.Module.FNF/DataModels/FnfSongData.cs
+++ Artemis.Plugins.Module.FNF/DataModels/FnfSongData.cs
@@ -1,12 +1,33 @@
-﻿using SkiaSharp;
+﻿using Artemis.Core.Modules;
+using SkiaSharp;
 
 namespace Artemis.Plugins.Module.FNF.DataModels {
     public class FnfSongData {
-        public SKColor DadHealthHex { get; set; } = new SKColor (0xB7, 0xD8, 0x55, 0xFF);
-        public SKColor BfHealthHex { get; set; } = new SKColor (0x31, 0xB0, 0xD1, 0xFF);
-        public SKColor LeftNoteHex { get; set; } = new SKColor (0xC2, 0x4B, 0x99, 0xFF);
-        public SKColor DownNoteHex { get; set; } = new SKColor (0x00, 0xFF, 0xFF, 0xFF);
-        public SKColor UpNoteHex { get; set; } = new SKColor (0x12, 0xFA, 0x05, 0xFF);
-        public SKColor RightNoteHex { get; set; } = new SKColor (0xF9, 0x39, 0x3F, 0xFF);
+        public SKColor DadHealthColor { get; set; } = new SKColor (0xB7, 0xD8, 0x55, 0xFF);
+        [DataModelIgnore]
+        public string DadHealthHex {
+            get => DadHealthColor.ToString ();
+            set {
+                SKColor val = DadHealthColor;
+                SKColor.TryParse (value, out val);
+                DadHealthColor = val;
+            }
+        }
+
+        public SKColor BfHealthColor { get; set; } = new SKColor (0x31, 0xB0, 0xD1, 0xFF);
+        [DataModelIgnore]
+        public string BfHealthHex {
+            get => BfHealthColor.ToString ();
+            set {
+                SKColor val = BfHealthColor;
+                SKColor.TryParse (value, out val);
+                BfHealthColor = val;
+            }
+        }
+
+        public SKColor LeftNoteColor { get; set; } = new SKColor (0xC2, 0x4B, 0x99, 0xFF);
+        public SKColor DownNoteColor { get; set; } = new SKColor (0x00, 0xFF, 0xFF, 0xFF);
+        public SKColor UpNoteColor { get; set; } = new SKColor (0x12, 0xFA, 0x05, 0xFF);
+        public SKColor RightNoteColor { get; set; } = new SKColor (0xF9, 0x39, 0x3F, 0xFF);
     }
 }
