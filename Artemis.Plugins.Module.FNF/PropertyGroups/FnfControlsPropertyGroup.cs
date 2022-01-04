535a6263d205d0c36c0cb879a100bb9fb98dd08a
niko <skedgyedgy@gmail.com>
2022-01-03 19:21:10 -0500

    added new controls layer brush

---
+++ Artemis.Plugins.Module.FNF/PropertyGroups/FnfControlsPropertyGroup.cs
@@ -0,0 +1,14 @@
+﻿using Artemis.Core;
+
+namespace Artemis.Plugins.Module.FNF.PropertyGroups {
+    public class FnfControlsPropertyGroup : LayerPropertyGroup {
+        protected override void PopulateDefaults () {
+        }
+
+        protected override void EnableProperties () {
+        }
+
+        protected override void DisableProperties () {
+        }
+    }
+}
